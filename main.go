cf9e69989568c2216456fc953776248022bc112f
coffin <coffinxp.js@gmail.com>
2024-08-13 04:57:37 -0600

    Update main.go

--- main.go
+++ main.go
@@ -1,64 +1,73 @@
 package main
 
 import (
-	"sync"
 	"bufio"
-	"net/http"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"strings"
-	"io/ioutil"
+	"sync"
 )
 
-func main(){
-
+func main() {
 	colorReset := "\033[0m"
 	colorRed := "\033[31m"
-    colorGreen := "\033[32m"
-
+	colorGreen := "\033[32m"
+	colorBlue := "\033[34m"
 
 	sc := bufio.NewScanner(os.Stdin)
 
 	jobs := make(chan string)
 	var wg sync.WaitGroup
 
-	for i:= 0; i < 20; i++{
-
+	for i := 0; i < 20; i++ {
 		wg.Add(1)
-		go func(){
+		go func() {
 			defer wg.Done()
 			for domain := range jobs {
-
 				resp, err := http.Get(domain)
-				if err != nil{
+				if err != nil {
 					continue
 				}
 				body, err := ioutil.ReadAll(resp.Body)
 				if err != nil {
-	      			fmt.Println(err)
-	   			}
-	   			sb := string(body)
-	   			check_result := strings.Contains(sb , "alert(1)")
-	   			// fmt.Println(check_result)
-	   			if check_result != false {
-	   				fmt.Println(string(colorGreen),"XSS FOUND:", domain,string(colorReset))
-	   			}else{
-	   				fmt.Println(string(colorRed),"Not Vulnerable:", domain, string(colorReset))
-	   			}
+					fmt.Println(err)
+					continue
+				}
+				sb := string(body)
 
+				// Initialize counts
+				alertCount := strings.Count(sb, "alert(1)")
+				confirmCount := strings.Count(sb, "confirm(1)")
+				promptCount := strings.Count(sb, "prompt(1)")
+
+				// Check for alerts
+				if alertCount > 0 || confirmCount > 0 || promptCount > 0 {
+					fmt.Printf(
+						"%sXSS FOUND: %s\nAlert Count: %d, Confirm Count: %d, Prompt Count: %d%s\n",
+						colorGreen,
+						domain,
+						alertCount,
+						confirmCount,
+						promptCount,
+						colorReset,
+					)
+				} else {
+					fmt.Printf(
+						"%sNot Vulnerable: %s%s\n",
+						colorRed,
+						domain,
+						colorReset,
+					)
+				}
 			}
-			
-   		}()
-
+		}()
 	}
 
-
-
-	for sc.Scan(){
+	for sc.Scan() {
 		domain := sc.Text()
-		jobs <- domain		
-		
-
+		jobs <- domain
 	}
 	close(jobs)
 	wg.Wait()
